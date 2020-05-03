5a1c897fb2187ac3aee510acd55acb1862a8e09c
Ellis <ems236@case.edu>
2020-05-03 19:10:16 -0400

    Graph loaded

--- src/main/scala/SimpleApp.scala
+++ src/main/scala/SimpleApp.scala
@@ -1,15 +1,24 @@
 import org.apache.spark.graphx.Pregel
 import org.apache.spark.sql.SparkSession
-
+import org.apache.spark.graphx._
 
 object SimpleApp {
   def main(args: Array[String]) {
+
+    println(s"STARTING")
+
+
     val logFile = "/opt/spark-2.4.5-bin-hadoop2.7/README.md" // Should be some file on your system
     val spark = SparkSession.builder.appName("Simple Application").getOrCreate()
     val logData = spark.read.textFile(logFile).cache()
     val numAs = logData.filter(line => line.contains("a")).count()
     val numBs = logData.filter(line => line.contains("b")).count()
     println(s"Lines with a: $numAs, Lines with b: $numBs")
+
+    val graph = BitcoinDataLoader.bitcoinGraph(spark)
+    println(graph.vertices.count())
+    println(graph.edges.count())
+
     spark.stop()
   }
 }
