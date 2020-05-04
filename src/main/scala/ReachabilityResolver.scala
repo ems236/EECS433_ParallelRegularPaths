3a34204f67b703364b3fdfa83cd5636010859b95
Ellis <ems236@case.edu>
2020-05-04 02:06:33 -0400

    Vertex Discovery

--- src/main/scala/ReachabilityResolver.scala
+++ src/main/scala/ReachabilityResolver.scala
@@ -3,12 +3,13 @@ import org.apache.spark.graphx._
 import scala.collection.mutable
 import scala.collection.mutable.Map
 
+
 case class VertexPair(source: VertexId, dest: VertexId)
 case class ReachabilityQuery[VD, ED](sourceFilter: ((VertexId, VD)) => Boolean,
                                      destFilter: ((VertexId, VD)) => Boolean,
                                      pathExpression: Array[PathRegexTerm[ED]])
 
-case class TermStatus(isFrontier: Boolean, middlestIndex: Int, originIds: Set[VertexId])
+case class TermStatus(isFrontier: Boolean, middlestIndex: Int, originIds: mutable.Set[VertexId])
 
 object ReachabilityResolver
 {
@@ -19,11 +20,11 @@ object ReachabilityResolver
     val dests = graph.vertices.filter(reachabilityQuery.destFilter)
 
     val lastIndex = reachabilityQuery.pathExpression.length - 1
-    var source_set = sources.map(v => (v._1, TermStatus(true, -1, Set(v._1))))
+    var source_set = sources.map(v => (v._1, TermStatus(true, -1, mutable.Set(v._1))))
       .collectAsMap()
     var mutable_source_set = collection.mutable.Map(source_set.toSeq: _*)
 
-    var dest_set = dests.map(v => (v._1, TermStatus(true, lastIndex + 1, Set(v._1))))
+    var dest_set = dests.map(v => (v._1, TermStatus(true, lastIndex + 1, mutable.Set(v._1))))
       .collectAsMap()
     var mutable_dest_set = collection.mutable.Map(dest_set.toSeq: _*)
 
@@ -49,47 +50,80 @@ object ReachabilityResolver
       }
     }
 
-    return intersectResults(source_set, dest_set, currentBackward)
+    //return intersectResults(source_set, dest_set, currentBackward)
 
   }
 
   def expandFrontier[VD, ED](graph: Graph[VD, ED]
      , vertexMap: mutable.Map[VertexId, TermStatus]
      , regex: Array[PathRegexTerm[ED]]
-     , regexIndex: Int) =
+     , regexIndex: Int
+     , previousIndex: Int) =
   {
     //take all the frontiers
-    //Doing a lot of mutating, so better to not keep this as an RDD
-    val frontier = vertexMap.filter(v => v._2.isFrontier)
-    val newFrontier: Map[VertexId, TermStatus] = Map()
+    var frontier = vertexMap.filter(v => v._2.isFrontier).map(v => (v._1, v._2.originIds.toSet))
+    var newFrontier: mutable.Map[VertexId, Set[VertexId]] = mutable.Map()
     val regexTerm = regex(regexIndex)
-    val expandCount = if (regexTerm.hasLimit()) 1 else regexTerm.limitVal()
+    val hasLimit = regexTerm.hasLimit()
+    val expandCount = if (hasLimit) regexTerm.limitVal() else 0
 
     //do their expansion in a while loop until its done
     var currentCount = 0
-    while (currentCount < expandCount)
+    while (frontier.size > 0 && currentCount < expandCount)
     {
       for(vertex <- frontier)
       {
         val newVertices = graph.edges
-          .filter(e => e.srcId == vertex._1 && regexTerm.doesMatch(e.attr))
+          .filter(e => e.srcId == vertex && regexTerm.doesMatch(e.attr))
           .map(e => e.dstId)
           .distinct()
           .collect()
 
         for(newVertex <- newVertices) {
-          //if exists in vertex set, need to update it
-          //else add new vertex to vertex set
-
+          reachNewVertes(newVertex, vertexMap, vertex._2, regexIndex)
           //add to new frontier
+          newFrontier(newVertex) = vertexMap(newVertex).originIds.toSet
         }
       }
 
       //collect new frontier into frontier
+      frontier = newFrontier
+      newFrontier = mutable.Map()
     }
 
 
     //Clear all reachability info for non-frontier
+    clearOldFrontier(vertexMap, previousIndex)
+  }
+
+  def reachNewVertes(newVertex: VertexId, vertexMap: mutable.Map[VertexId, TermStatus], originSet:  Set[VertexId], regexIndex: Int) =
+  {
+    //if exists in vertex set, need to update it
+    if(vertexMap.contains(newVertex))
+    {
+      val oldTermSet = vertexMap(newVertex).originIds
+      oldTermSet ++= originSet
+      //copy same set over
+      val newTermStatus = TermStatus(true, regexIndex, oldTermSet)
+      vertexMap(newVertex) = newTermStatus
+    }
+    else
+    {
+      //else add new vertex to vertex set
+      val newSet = mutable.Set[VertexId]()
+      newSet ++= originSet
+      val newTermStatus = TermStatus(true, regexIndex, newSet)
+      vertexMap(newVertex) = newTermStatus
+    }
+  }
+
+  def clearOldFrontier(vertexMap: mutable.Map[VertexId, TermStatus], clearIndex: Int): Unit =
+  {
+    val oldFrontiers = vertexMap.filter(v => v._2.middlestIndex == clearIndex).keySet
+    for(id <- oldFrontiers)
+    {
+      vertexMap(id) = TermStatus(false, clearIndex, mutable.Set())
+    }
   }
 
   def intersectResults(sourceSet: VertexRDD[TermStatus], destSet: VertexRDD[TermStatus], meetIndex: Int): Array[VertexPair] =
