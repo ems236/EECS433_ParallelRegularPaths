47bd915b7754310157a797244da1a24d3807fcae
Ellis <ems236@case.edu>
2020-05-03 23:40:24 -0400

    pseudocode

--- src/main/scala/ReachabilityResolver.scala
+++ src/main/scala/ReachabilityResolver.scala
@@ -16,13 +16,23 @@ object ReachabilityResolver
     val dests = graph.vertices.filter(reachabilityQuery.destFilter)
 
     val lastIndex = reachabilityQuery.pathExpression.length - 1
-    val source_set = VertexRDD[TermStatus](sources.map(v => (v._1, TermStatus(true, 0, lastIndex + 1, List(v._1)))))
-    val dest_set = VertexRDD[TermStatus](dests.map(v => (v._1, TermStatus(true, -1, lastIndex, List(v._1)))))
+    val source_set = VertexRDD[TermStatus](sources.map(v => (v._1, TermStatus(true, 0, lastIndex + 2, List(v._1)))))
+    val dest_set = VertexRDD[TermStatus](dests.map(v => (v._1, TermStatus(true, -1, lastIndex + 1, List(v._1)))))
 
+    //Start 1 off because not moving through the graph yet
+    val currentForward = -1
+    val currentBackward = lastIndex + 1
+
+    while(currentForward < currentBackward)
+    {
+      
+    }
   }
 
   def expandedFrontier[VD, ED](graph: Graph[VD, ED], vertexSet: VertexRDD[TermStatus]) : VertexRDD[TermStatus] =
   {
-    
+    //take all the frontiers
+    //do their expansion in a while loop until its done
+      //move all the source nodes forward as you do this
   }
 }
