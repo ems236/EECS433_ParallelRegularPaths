ac7e7450cdb3e04b15196897f95e818879e8d43b
Ellis <ems236@case.edu>
2020-05-03 23:24:40 -0400

    more BFS

--- src/main/scala/ReachabilityResolver.scala
+++ src/main/scala/ReachabilityResolver.scala
@@ -5,16 +5,24 @@ case class ReachabilityQuery[VD, ED](sourceFilter: ((VertexId, VD)) => Boolean,
                                      destFilter: ((VertexId, VD)) => Boolean,
                                      pathExpression: List[PathRegexTerm[ED]])
 
-class VertexStatus[VD]()
+case class TermStatus(isFrontier: Boolean, maxForwardIndex: Int, minBackwardsIndex: Int, originIds: List[VertexId])
 
 object ReachabilityResolver
 {
-  def ResolveQuery[VD, ED](graph: Graph[VD, ED], reachabilityQuery: ReachabilityQuery[VD, ED]) : List[VertexPair] =
+  def ResolveQuery[VD, ED](graph: Graph[VD, ED], reachabilityQuery: ReachabilityQuery[VD, ED]) : List[VertexId] =
   {
     //Bi-directional search
-    val source_set = graph.vertices.filter(reachabilityQuery.sourceFilter)
-    val dest_set = graph.vertices.filter(reachabilityQuery.destFilter)
+    val sources = graph.vertices.filter(reachabilityQuery.sourceFilter)
+    val dests = graph.vertices.filter(reachabilityQuery.destFilter)
+
+    val lastIndex = reachabilityQuery.pathExpression.length - 1
+    val source_set = VertexRDD[TermStatus](sources.map(v => (v._1, TermStatus(true, 0, lastIndex + 1, List(v._1)))))
+    val dest_set = VertexRDD[TermStatus](dests.map(v => (v._1, TermStatus(true, -1, lastIndex, List(v._1)))))
+
   }
 
-  def
+  def expandedFrontier[VD, ED](graph: Graph[VD, ED], vertexSet: VertexRDD[TermStatus]) : VertexRDD[TermStatus] =
+  {
+    
+  }
 }
